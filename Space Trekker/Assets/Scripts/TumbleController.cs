bd5a1d37b5eb3b4aa18a0bff2370b8071db112da
Jason Fry <funkytomato@gmail.com>
2018-12-31 20:36:12 +0000

    Created Asteroid

---
+++ Space Trekker/Assets/Scripts/TumbleController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TumbleController : MonoBehaviour
+{
+
+    public float Tumble;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * Tumble;
+    }
+}
