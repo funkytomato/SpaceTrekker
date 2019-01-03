e5c8577b2c5e0b3fd49d1260b0caf875ead25c58
Jason Fry <funkytomato@gmail.com>
2019-01-03 22:05:11 +0000

    Creates GameObject at a random position within the given boundary gameobject.

--- Space Trekker/Assets/Scripts/RandomStartPosition.cs
+++ Space Trekker/Assets/Scripts/RandomStartPosition.cs
@@ -6,11 +6,22 @@ public class RandomStartPosition : MonoBehaviour
 {
 
     public GameObject prefab;
+    public GameObject Boundary; 
 
     // Instantiate the Prefab somewhere between -10.0 and 10.0 on the x-z plane
     void Start()
     {
-        Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+
+
+        //Instantiate(gameObject, Vector3(Random.Range(minY, maxY),
+        //Random.Range(minZ, maxZ),
+        //Random.Range(minX, maxX), Quaternion.identity));
+
+
+
+        Vector3 position = RandomPointInBox(Vector3.zero, Boundary.transform.localScale);
+
+        //Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
         Instantiate(prefab, position, Quaternion.identity);
     }
 
@@ -18,5 +29,15 @@ public class RandomStartPosition : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private static Vector3 RandomPointInBox(Vector3 center, Vector3 size)
+    {
+
+        return center + new Vector3(
+           (Random.value - 0.5f) * size.x,
+           (Random.value - 0.5f) * size.y,
+           (Random.value - 0.5f) * size.z
+        );
     }
 }
